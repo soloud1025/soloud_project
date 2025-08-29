0bfe26afec61ba8b6fc56812de4691f5169947ee
soloud1025 <soloud1025@gmail.com>
2025-08-29 15:25:28 +0900

    Create app.py

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello from soloud_project Flask WAS!"
+
+if __name__ == "__main__":
+    # 로컬 테스트용 (EKS 배포 시 gunicorn으로 실행)
+    app.run(host="0.0.0.0", port=5000)
